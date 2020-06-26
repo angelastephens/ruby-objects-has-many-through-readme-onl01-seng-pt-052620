7437930627157b4a46ce8ab630fef61068014e9b
Angela Stephens <angelastephens317@gmail.com>
2020-06-26 02:19:13 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,18 @@
 class Waiter
+  
+  attr_accessor :name
+  
+  @@all=[]
+  
+  def initialize(name,years)
+    @name=name
+    @years=years
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
