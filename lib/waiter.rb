d58d75d2181523e1ce04f9f77842eb1c99ec6dfc
Angela Stephens <angelastephens317@gmail.com>
2020-06-26 02:57:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,9 +19,9 @@ class Waiter
     Meal.new(self,customer,total, tip)
   end
   
-  def waiters
-    meals.map do |meal|
-      meal.waiter
+   def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
     end
   end
 
