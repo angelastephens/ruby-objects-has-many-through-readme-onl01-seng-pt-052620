c1f303a2112723283ae66b771fa01e66c35e0bcf
Angela Stephens <angelastephens317@gmail.com>
2020-06-26 02:35:23 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -26,12 +26,12 @@ class Waiter
   
   
   
-  def best_tipper
-  best_tipped_meal = Meal.max do |meal_a, meal_b|
-    meal_a.tip <=> meal_b.tip
-  end
+#   def best_tipper
+#   best_tipped_meal = Meal.max do |meal_a, meal_b|
+#     meal_a.tip <=> meal_b.tip
+#   end
  
-  best_tipped_meal.customer
-end
+#   best_tipped_meal.customer
+# end
 
 end
